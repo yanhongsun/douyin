c7ecf37033e41ebb84bdfdf784a308d6fa1b13f3
barars <1286561513@qq.com>
2022-06-02 13:34:13 +0800

    merge prepare

--- database/insert.sql
+++ database/insert.sql
@@ -5,6 +5,10 @@ INSERT INTO
 VALUES('11111111111@qq.com', 'douyin', 0, 1),('22222222222@qq.com', 'douyin', 1, 0);
 
 INSERT INTO
+    users(u_name, passwd, follow_count, fans_count)
+VALUES('333333333@qq.com', 'douyin', 0, 0),('444444444444@qq.com', 'douyin', 0, 0);
+
+INSERT INTO
     relation(follower1, follower2, tag)
 VALUES(1, 2, 1);
 
