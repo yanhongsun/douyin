6c8b894a28afd5ba7ae4c0edb97d5259570a2816
barars <1286561513@qq.com>
2022-06-04 19:46:05 +0800

    fix some bug in api

--- cmd/api/handlers/handler.go
+++ cmd/api/handlers/handler.go
@@ -168,9 +168,9 @@ func SendMultiUserResponse(c *gin.Context, err error, userInfos []*user.User) {
 }
 
 type LikeyouParam struct {
-	UserId     int64 `json:"user_id"`
-	VideoId    int64 `json:"video_id"`
-	ActionType int64 `json:"action_type"`
+	Token      string `json:"token"`
+	VideoId    string `json:"video_id"`
+	ActionType string `json:"action_type"`
 }
 type ResponseThumb struct {
 	Code    int32       `json:"code"`
